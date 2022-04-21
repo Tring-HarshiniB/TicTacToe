cfbf348cab1cdbbf20428364bb4dc4f1e360c147
tringapps-harshini <102798974+tringapps-harshini@users.noreply.github.com>
2022-04-21 15:02:51 +0530

    Delete Evaluation.java

--- Evaluation.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public enum Evaluation {
-    InvalidInput,
-    Draw, // valid board, but the game hasn't been won by either player yet 
-    X_wins, // valid board, and the game was won by player X 
-    O_wins, // valid board, and the game was won by player O 
-    UnreachableState // string describes a board, but not a board state that is reachable from a valid game 
-}
